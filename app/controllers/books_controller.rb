ddeb2375fb4a1026b233961f0457e31acda7a250
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2023-01-28 20:36:25 +0000

    [add]編集画面と更新機能の実装まで_投稿機能・ルート機能がエラー検出

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,6 +30,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "You have updated Book successfully."
     redirect_to book_path(book.id)  
   end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
   def show
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
   
   def index
@@ -9,12 +9,18 @@ class UsersController < ApplicationController
   end
   
   def edit
+    @user = User.find(current_user.id)
   end
   
   def update
     user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(current_user.id)
+    if user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(current_user.id)
+    else
+      flash[:alert] = "You have not updated user successfully."
+      render edit_user_path(current_user.id)
+    end
   end
   
   private
