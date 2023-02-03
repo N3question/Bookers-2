1c0260b5e933214b92ef1cc721b112de6dd2e5b8
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2023-02-03 20:14:58 +0000

    [add]ヘッダーのアイコンを追加_aタグを使用_未完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!, except: [:top] # ログインしていないと他のページにアクセス不可、ログイン画面へリダイレクト
+    before_action :authenticate_user!, except: [:top, :about] # ログインしていないと他のページにアクセス不可、ログイン画面へリダイレクト
     before_action :configure_permitted_parameters, if: :devise_controller?
     
     def after_sign_in_path_for(resource)
